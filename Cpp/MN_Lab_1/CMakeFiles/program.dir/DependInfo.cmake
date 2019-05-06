9fa8c6fe45c4ce66d79c3a1f50d7880d93374472
rzepkamichal <michrzep@interia.pl>
2019-05-06 22:07:42 +0200

    sprawdzone wyniki, wersja ostateczna, OK

--- Cpp/MN_Lab_1/CMakeFiles/program.dir/DependInfo.cmake
+++ Cpp/MN_Lab_1/CMakeFiles/program.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/muser/Desktop/dev/Learning/Cpp/MN_Lab_1/src/Main.cpp" "/home/muser/Desktop/dev/Learning/Cpp/MN_Lab_1/CMakeFiles/MN_Lab_1.dir/src/Main.cpp.o"
+  "/home/musr/Desktop/dev/Learning/Cpp/MN_Lab_1/src/Main.cpp" "/home/musr/Desktop/dev/Learning/Cpp/MN_Lab_1/CMakeFiles/program.dir/src/Main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
