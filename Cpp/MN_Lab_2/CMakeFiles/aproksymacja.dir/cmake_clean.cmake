9a82ec2a53586dd44a332043174c43a9dc35e9aa
rzepkamichal <michrzep@interia.pl>
2019-05-08 22:14:02 +0200

    upload project aproksymacja

---
+++ Cpp/MN_Lab_2/CMakeFiles/aproksymacja.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aproksymacja.dir/src/main.cpp.o"
+  "aproksymacja.pdb"
+  "aproksymacja"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/aproksymacja.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
